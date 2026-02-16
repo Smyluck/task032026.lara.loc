3e3b86c1520f790e0cd541b6e4d8dcfc240de736
Smyluck <veb-master.veb@yandex.ru>
2026-02-16 08:00:42 +0000

    Настройки с таймвеб

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
 
         Setting::create([
             'name' => 'Подключить Яндекс',
